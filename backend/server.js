9f9b57299779e8c6772f4edd1f3c8b5b78575e6d
su-yash-7 <suyashpradhan7@gmail.com>
2021-07-19 20:08:37 +0530

    Basic API EndPoint

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const notes = require('./data/notes');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('API is Running..');
+});
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+app.get('/api/notes/:id', (req, res) => {
+  const note = notes.find((n) => n._id === req.params.id);
+  res.json(note);
+});
 app.listen(5000, console.log('Server Started on PORT 5000'));
