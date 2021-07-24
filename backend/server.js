70d7284101aabb31a8b92261b163ab86108c54c7
su-yash-7 <suyashpradhan7@gmail.com>
2021-07-24 10:38:18 +0530

    Created Backend Registration API

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,22 @@ const express = require('express');
 const notes = require('./data/notes');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes.js');
+const { notFound, errorHandler } = require('./middleware/errorMiddleware');
+
 const app = express();
 dotenv.config();
 connectDB();
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('API is Running..');
 });
 app.get('/api/notes', (req, res) => {
   res.json(notes);
 });
-app.get('/api/notes/:id', (req, res) => {
-  const note = notes.find((n) => n._id === req.params.id);
-  res.json(note);
-});
-
+app.use('/api/users', userRoutes);
+app.use(notFound);
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server Started on PORT ${PORT}`));
