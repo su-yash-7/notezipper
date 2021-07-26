61b0eeb80984d6220ff1b35889fa5f2ba86e1528
su-yash-7 <suyashpradhan7@gmail.com>
2021-07-27 00:39:32 +0530

    DELETE - Note by ID

--- backend/controller/noteController.js
+++ backend/controller/noteController.js
@@ -51,5 +51,20 @@ const UpdateNote = asyncHandler(async (req, res) => {
     throw new Error('Note not found');
   }
 });
+const DeleteNote = asyncHandler(async (req, res) => {
+  const note = await Note.findById(req.params.id);
 
-module.exports = { getNotes, CreateNote, getNoteById, UpdateNote };
+  if (note.user.toString() !== req.user._id.toString()) {
+    res.status(401);
+    throw new Error("You can't perform this action");
+  }
+
+  if (note) {
+    await note.remove();
+    res.json({ message: 'Note Removed' });
+  } else {
+    res.status(404);
+    throw new Error('Note not Found');
+  }
+});
+module.exports = { getNotes, CreateNote, getNoteById, UpdateNote, DeleteNote };
--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -4,13 +4,17 @@ const {
   CreateNote,
   getNoteById,
   UpdateNote,
+  DeleteNote,
 } = require('../controller/noteController');
 const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.route('/').get(protect, getNotes);
 router.route('/create').post(protect, CreateNote);
-router.route('/:id').get(getNoteById).put(protect, UpdateNote);
-//   .delete(protect, DeleteNote);
+router
+  .route('/:id')
+  .get(getNoteById)
+  .put(protect, UpdateNote)
+  .delete(protect, DeleteNote);
 
 module.exports = router;
