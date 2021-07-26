c516373fdee79675efb3b4382dfa37b516e21f31
su-yash-7 <suyashpradhan7@gmail.com>
2021-07-26 16:41:27 +0530

    Redux Registration Action

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Col, Container, Form, Row } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import MainScreen from '../components/MainScreen';
 import styled from 'styled-components';
-import axios from 'axios';
 import ErrorMessage from '../components/ErrorMessage';
 import Loading from '../components/Loading';
+import { useDispatch, useSelector } from 'react-redux';
+import { register } from '../actions/userActions';
 
 function RegisterScreen() {
   const [email, setEmail] = useState('');
@@ -17,41 +18,13 @@ function RegisterScreen() {
   const [confirmpassword, setConfirmPassword] = useState('');
   const [message, setMessage] = useState(null);
   const [picMessage, setPicMessage] = useState(null);
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
 
-  const submitHandler = async (e) => {
-    e.preventDefault();
-    if (password !== confirmpassword) {
-      setMessage('Password Do not Match');
-    } else {
-      setMessage(null);
-      try {
-        const config = {
-          headers: {
-            'Content-type': 'application/json',
-          },
-        };
-        setLoading(true);
-        const { data } = await axios.post(
-          '/api/users',
-          {
-            name,
-            email,
-            password,
-            pic,
-          },
-          config
-        );
-        console.log(data);
-        localStorage.setItem('userInfo', JSON.stringify(data));
-        setLoading(false);
-      } catch (error) {
-        setLoading(false);
-        setError(error.response.data.message);
-      }
-    }
-  };
+  const userRegister = useSelector((state) => state.userRegister);
+  const { loading, error, userInfo } = userRegister;
+
+  const history = useHistory();
+
   const postDetails = (pics) => {
     if (!pics) {
       return setPicMessage('Please Select an Image');
@@ -76,6 +49,19 @@ function RegisterScreen() {
         });
     } else {
       return setPicMessage('Please Select an Image');
+    }
+  };
+  useEffect(() => {
+    if (userInfo) {
+      history.push('/mynotes');
+    }
+  }, [history, userInfo]);
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (password !== confirmpassword) {
+      setMessage('Passwords do not match');
+    } else {
+      dispatch(register(name, email, password, pic));
     }
   };
   return (
