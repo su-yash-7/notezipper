954445ee49f0122c942b7f6db4bf4508915185b4
su-yash-7 <suyashpradhan7@gmail.com>
2021-07-28 16:46:46 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const userRoutes = require('./routes/userRoutes');
 const noteRoutes = require('./routes/noteRoutes');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 const cors = require('cors');
-
+const path = require('path');
 const app = express();
 app.use(
   cors({
@@ -18,14 +18,24 @@ dotenv.config();
 connectDB();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is Running..');
-});
-// app.get('/api/notes', (req, res) => {
-//   res.json(notes);
-// });
 app.use('/api/users', userRoutes);
 app.use('/api/notes', noteRoutes);
+// ----------Deployment----------
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running..');
+  });
+}
+// // ------------------------------
+
 app.use(notFound);
 app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
